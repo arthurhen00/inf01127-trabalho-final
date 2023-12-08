06ae70b3c2019107c0ebbd46e72f01f66007eb5f
ArthurHendges <arthurhendges@hotmail.com>
2023-12-08 20:00:24 -0300

    minimal changes

--- web/src/app/home/page.tsx
+++ web/src/app/home/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
       <>
         {/** Header */}
-        <div className='bg-gray-300 flex flex-row justify-between px-24 py-4 border-b-2 border-gray-200'>
+        <div className='bg-gray-100 flex flex-row justify-between px-24 py-4 border-b-2 border-gray-200'>
           <div className='flex items-center'>
             <BiBuildingHouse className="w-10 h-10"/>
             <PiHouseLineBold className="w-6 h-6"/>
@@ -33,14 +33,15 @@ export default function Home() {
         </div>
 
         {/** Menu */}
-        <div className='flex px-24 py-2 justify-center bg-gray-300 '>
-            <a href='' className='px-6 py-2 border-b-2 border-gray-300 hover:border-gray-200'>Perfil</a>
-            <a href='' className='px-6 py-2 border-b-2 border-gray-300 hover:border-gray-200'>Buscar</a>
-            <a href='' className='px-6 py-2 border-b-2 border-gray-300 hover:border-gray-200'>Minhas propriedades</a>
+        <div className='flex px-24 py-2 justify-center bg-gray-100 '>
+            <a href='' className='px-6 py-2 border-b-2 border-gray-100 hover:border-gray-200'>Perfil</a>
+            <a href='' className='px-6 py-2 border-b-2 border-gray-100 hover:border-gray-200'>Buscar</a>
+            <a href='' className='px-6 py-2 border-b-2 border-gray-100 hover:border-gray-200'>Minhas propriedades</a>
+            <a href='' className='px-6 py-2 border-b-2 border-gray-100 hover:border-gray-200'>Pedidos</a>
         </div>
 
-        <main className='bg-gray-200'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et alias deleniti repudiandae minima sit a modi maxime reiciendis ut omnis velit, cumque laborum amet soluta ipsum excepturi facilis nisi. Optio?
+        <main className='bg-gray-100 px-24'>
+          main content here
         </main>
       </>
   )
