10c979b0ce6b9639a84408d895015751a7e707b4
ArthurHendges <arthurhendges@hotmail.com>
2023-12-13 17:38:23 -0300

    rota de upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart  from '@fastify/multipart'
 import { usersRoutes } from './routes/users'
 import { propertiesRoutes } from './routes/properties'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 
@@ -14,8 +16,11 @@ app.register(jwt, {
     secret: 'swipehome-h178dg187shdu19dn19dg6218dogdqdg687',
 })
 
+app.register(multipart)
+
 app.register(usersRoutes)
 app.register(propertiesRoutes)
+app.register(uploadRoutes)
 
 app.get('/', () => {
     return 'kkk rodou'
