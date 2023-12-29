04699ccb1abcad63518fbf4e2ab66edd4aa1cf72
arthurhendges <arthurhendges@hotmail.com>
2023-12-29 13:06:13 -0300

    limpando banco

--- server/src/routes/match.ts
+++ server/src/routes/match.ts
@@ -35,7 +35,7 @@ export async function matchRoutes(app: FastifyInstance) {
                 receiverId: request.user.sub,
             },
             orderBy: {
-                createdAt: 'asc',
+                propertyId: 'asc',
             },
         })
 
