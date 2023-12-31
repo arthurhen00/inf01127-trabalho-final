c6242cbacd50d9ce8c96776746e2422cf82ecf1b
arthurhendges <arthurhendges@hotmail.com>
2023-12-31 20:28:10 -0300

    estilizacao unauthenticated component

--- web/src/components/Unauthenticated.tsx
+++ web/src/components/Unauthenticated.tsx
@@ -1,8 +1,8 @@
 export default function Unauthenticated() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-300">
-      <p className="text-2xl font-bold mb-4">Não autenticado, faça login</p>
-      <a href="/" className="text-blue-500 hover:underline">Página de login</a>
+      <p className="text-4xl font-bold mb-4">Não autenticado, faça login</p>
+      <a href="/" className="text-blue-500 hover:underline text-lg">Página de login</a>
     </div>
   )
 }
